a18608e0d9860360f6542be28c056763d6b4445d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2011-11-08 22:20:29 +0200

    org-file function for referencing files in ~/Dropbox/org-files.

--- .emacs.d/orgrc.el
+++ .emacs.d/orgrc.el
@@ -7,12 +7,17 @@
 (global-set-key (kbd "C-M-r") 'remember)
 (global-set-key (kbd "C-c l") 'org-store-link)
 
+(setq org-directory "~/Dropbox/org-files")
+(defun org-file (fname)
+  (concat org-directory "/" fname ".org"))
+
 (setq
  org-agenda-restore-windows-after-quit t
  org-replace-disputed-keys t
- org-agenda-files '("~/Dropbox/org-files/todo.org" "~/Dropbox/org-files/birthdays.org")
+ org-agenda-files (mapcar 'org-file '("todo" "birthdays"))
  org-agenda-ndays 7
  org-deadline-warning-days 14
+ org-default-notes-file (org-file "todo")
  org-agenda-show-all-dates t
  org-agenda-skip-deadline-if-done t
  org-agenda-skip-scheduled-if-done t
@@ -21,9 +26,9 @@
  org-fast-tag-selection-single-key 'expert
  org-remember-store-without-prompt t
  org-remember-templates (quote
-                         ((97  "* APPT %?\n  %u"     "~/Dropbox/org-files/todo.org" "Tasks")
-                          (84  "* TODO %?\n  %u\n%a" "~/Dropbox/org-files/todo.org" "Tasks")
-                          (116 "* TODO %?\n  %u"     "~/Dropbox/org-files/todo.org" "Tasks")))
+                         ((97  "* APPT %?\n  %u"     (org-file "todo") "Tasks")
+                          (84  "* TODO %?\n  %u\n%a" (org-file "todo") "Tasks")
+                          (116 "* TODO %?\n  %u"     (org-file "todo") "Tasks")))
  remember-annotation-functions '(org-remember-annotation)
  remember-handler-functions '(org-remember-handler)
  org-agenda-custom-commands
