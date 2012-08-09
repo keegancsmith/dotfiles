71d1c4a00b2e770f15aacaa46a5a78055e24de54
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2012-08-09 12:57:07 +0200

    Add source for hgallpaths.py to repo.

--- misc/hgallpaths.py
+++ misc/hgallpaths.py
@@ -1,1 +1,44 @@
-/home/keegan/repos/hgallpaths/hgallpaths.py
+# hgallpaths.py - pull and push to all paths
+#
+# Copyright 2012 Keegan Carruthers-Smith <keegan.csmith@gmail.com>
+#
+# Released under the terms of the BSD License. See LICENSE.txt for details.
+
+'''push and pull to all paths'''
+
+from mercurial.i18n import _
+from mercurial import commands, util
+
+cmdtable = {}
+
+
+def do_command(command, path_kw, ui, *args, **opts):
+    cmd = getattr(commands, command)
+    exclude = set(ui.configlist('hgallpaths', 'exclude', []) +
+                  ui.configlist('hgallpaths', 'exclude_%s' % command, []))
+    paths = ui.configitems('paths')
+    paths = [(name, path) for name, path in paths if name not in exclude]
+    path_names = ', '.join(name for name, path in paths)
+
+    if paths:
+        ui.status(_('%s to %s\n') % (command, path_names))
+    else:
+        raise util.Abort(_('No paths to %s') % command)
+
+    for name, path in paths:
+        if name not in exclude:
+            opts[path_kw] = path
+            cmd(ui, *args, **opts)
+
+
+def create_command(command, path_kw):
+    def cmd(*args, **opts):
+        return do_command(command, path_kw, *args, **opts)
+    cmd.__doc__ = 'See help for %s' % command
+
+    global cmdtable
+    cmdtable[command + 'all'] = (cmd, [])
+    return cmd
+
+pullall = create_command('pull', 'source')
+pushall = create_command('push', 'dest')
