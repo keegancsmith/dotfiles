d97d16813ec86452929dba8c2436e2fe37b72fe8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-10-28 19:34:13 +0200

    emacs: use dired-open for videos

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -423,6 +423,14 @@
 ; Highlight code when exporting
 (use-package htmlize)
 
+(use-package dired-open
+  :config
+  (setq dired-open-extensions
+        '(
+          ("mp4" . "mpv")
+          ("mkv" . "mpv")
+          )))
+
 (use-package direnv
  :config
  (direnv-mode))
