77f0982607e75559bba5764a37d34a824ccd5f2e
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-04-06 15:17:10 +0200

    qutebrowser: handle gdocs in org-capture.py

--- qutebrowser/userscripts/org-capture.py
+++ qutebrowser/userscripts/org-capture.py
@@ -14,7 +14,7 @@ def remove_prefix(s, prefix):
         return s[len(prefix):]
     return s
 
-def org_headline(url, title):
+def org_headline_github(url, title):
     p = url.split('/')
     if len(p) < 7 or p[2] != 'github.com' or p[5] not in ('pull', 'issues'):
         return None
@@ -34,6 +34,26 @@ def org_headline(url, title):
 
     return f'{title} [[{url}][{repo}#{pid}]]'
 
+def org_headline_gdocs(url, title):
+    m = re.match(r'RFC (\d+)[^:]*: (.+) - Google Docs', title)
+    if m is None:
+        return None
+
+    rfc = m.group(1)
+    title = m.group(2).strip()
+
+    if (idx := url.index('/edit')) >= 0:
+        url = url[:idx]
+
+    return f'RFC {rfc}: {title} [[{url}][RFC {rfc}]]'
+
+def org_headline(url, title):
+    for f in (org_headline_github, org_headline_gdocs):
+        org = f(url, title)
+        if org is not None:
+            return org
+    return org
+
 def test_org_headline():
     cases = ((
         'https://github.com/sourcegraph/zoekt/pull/10',
@@ -43,6 +63,10 @@ def test_org_headline():
         'https://github.com/sourcegraph/sourcegraph/issues/6031',
         'Core Services: 3.10 tracking issue · Issue #6031 · sourcegraph/sourcegraph',
         'Core Services: 3.10 tracking issue [[https://github.com/sourcegraph/sourcegraph/issues/6031][#6031]]',
+    ), (
+        'https://docs.google.com/document/d/18w8T_KzYxQye8wg1g01QpMOX4_ERTtbOxMBRYaOEkmk/edit#heading=h.rf2d3v2oo71l',
+        'RFC 30: Zoekt Horizontal Scaling - Google Docs',
+        'RFC 30: Zoekt Horizontal Scaling [[https://docs.google.com/document/d/18w8T_KzYxQye8wg1g01QpMOX4_ERTtbOxMBRYaOEkmk][RFC 30]]',
     ))
     for url, title, want in cases:
         got = org_headline(url, title)
