e83226bb63123c8e9156f3f6ad4d297352a9eb7d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-05-24 12:11:02 +0200

    emacs: switch to vertico and orderless for completion

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -175,14 +175,42 @@
   ; just hack at stuff.
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
-(use-package selectrum
-  :init
-  (selectrum-mode +1))
+(straight-register-package
+ '(vertico :host github :repo "minad/vertico"))
 
-(use-package selectrum-prescient
+(use-package vertico
   :init
-  (selectrum-prescient-mode +1)
-  (prescient-persist-mode +1))
+  (vertico-mode))
+
+;; Use the `orderless' completion style.
+;; Enable `partial-completion' for files to allow path expansion.
+;; You may prefer to use `initials' instead of `partial-completion'.
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles . (partial-completion))))))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations for completing-read
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
 
 (use-package marginalia
   :bind (("M-A" . marginalia-cycle)
