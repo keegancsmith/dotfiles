66134bafecc6ae81a7fcb75ae48c2102aa08f1ff
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-07-29 23:09:08 +0200

    nixos: add configuration.nix for habitat machine

---
+++ hosts/habitat/configuration.nix
@@ -0,0 +1,208 @@
+{ config, pkgs, ... }:
+
+let spotifydMpris = (pkgs.spotifyd.override { withMpris = true; });
+in {
+  imports = [ ./hardware-configuration.nix ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # append entries for other OSs detected by os-prober.
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "habitat";
+
+  # Set your time zone.
+  time.timeZone = "Africa/Johannesburg";
+
+  networking.useDHCP = false;
+  networking.interfaces.enp5s0.useDHCP = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  hardware.opengl.enable = true;
+
+  services.xserver = {
+    enable = true;
+
+    layout = "us";
+
+    xkbOptions = "caps:escape";
+
+    videoDrivers = [ "nvidia" ];
+
+    desktopManager = { xterm.enable = false; };
+
+    displayManager = { defaultSession = "none+i3"; };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [ rofi dmenu i3status i3lock ];
+    };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  users.users.keegan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "sound" ];
+  };
+
+  # build newer emacs with native-comp and pgtk. overlay sha commit date: 14
+  # July 2021
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/emacs-overlay/archive/f09b310259dded3d9c0ff4ff76a63999c055a2dd.tar.gz";
+    }))
+  ];
+
+  # google-chrome is unfree.
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+    git
+    go
+    direnv
+    gopls
+    htop
+    emacsPgtkGcc
+    notmuch
+    ripgrep
+    bash
+    starship
+    qutebrowser
+    google-chrome
+    spotify
+    slack
+    mpv
+    ledger
+    jq
+    fzf
+    fd
+    nmap
+    (pass.withExtensions (ext: [ ext.pass-otp ]))
+    rofi-pass
+    screen
+    watchman
+    youtube-dl
+    k9s
+    gcc
+    mosh
+    maim
+    alacritty
+    kubectl
+    google-cloud-sdk
+    lsof
+    spotifydMpris
+    nixfmt
+    zoom-us
+  ];
+
+  fonts.fonts = with pkgs; [ hack-font go-font ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.neovim.enable = true;
+  programs.neovim.viAlias = true;
+  programs.neovim.vimAlias = true;
+
+  documentation.dev.enable = true;
+
+  services.emacs.package = pkgs.emacsPgtkGcc;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.tailscale.enable = true;
+
+  services.unbound.enable = true;
+
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.avahi.publish.addresses = true;
+  services.avahi.publish.domain = true;
+  services.avahi.publish.enable = true;
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  # Default is performance. This seems like a more reasonable default, even
+  # though this is a desktop machine.
+  powerManagement.cpuFreqGovernor = "schedutil";
+
+  nix.allowedUsers = [ "@wheel" ];
+
+  virtualisation.docker.enable = true;
+
+  # TODO this doesn't work. Try out a user service but I read bad things about
+  # them that I don't yet understand. If that doesn't work, try out
+  # home-manager.
+  #
+  #systemd.services.spotifyd = {
+  #    wantedBy = [ "multi-user.target" ];
+  #    after = [ "network.target" "sound.target" ];
+  #    wants = [ "network.target" "sound.target" ];
+  #    description = "Start spotifyd.";
+  #    serviceConfig = {
+  #      User = "keegan";
+  #      ExecStart = ''${spotifydMpris}/bin/spotifyd --no-daemon --zeroconf-port 8701'';
+  #      Restart = "always";
+  #      RestartSec = "12";
+  #    };
+  # };
+
+  networking.firewall = {
+
+    allowedTCPPorts = [
+      # spotifyd zeroconf port
+      8701
+    ];
+
+    # Limit most stuff to tailscale network
+    interfaces.tailscale0 = {
+      allowedTCPPorts = [
+        # rclone webdav port for org-files <-> beorg
+        8780
+      ];
+
+      allowedUDPPortRanges = [
+        # mosh 
+        {
+          from = 60000;
+          to = 60010;
+        }
+      ];
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
