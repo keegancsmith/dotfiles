bb2b0de1cc775fb50d6ea3644194f48d62dad43b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-02-01 10:29:03 +0200

    org: feb journal

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -513,14 +513,14 @@
   (require 'ob-awk)
   (require 'ob-python)
 
-  (setq current-journal-filename "~/org-files/journals/2022/2022-01-Jan.org")
+  (setq current-journal-filename "~/org-files/journals/2022/2022-02-Feb.org")
 
   (setq
    org-agenda-files '("~/org-files"
                       "~/org-files/journals/2021"
                       "~/org-files/journals/2022")
    org-refile-targets '((("~/org-files/work.org" "~/org-files/home.org" "~/org-files/backlog.org" "~/org-files/notes.org" "~/org-files/learn.org") :maxlevel . 1)
-                        (("~/org-files/journals/2022/2022-01-Jan.org") :maxlevel . 1))
+                        (("~/org-files/journals/2022/2022-02-Feb.org") :maxlevel . 1))
    org-archive-location "%s_archive::datetree/"
    org-deadline-warning-days 14
    org-default-notes-file "~/org-files/inbox.org"
