adba4b0227e2160d5d473fd1a3449998897e03ff
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-12-01 14:33:30 +0200

    nixos: enableNsncd

--- hosts/habitat/configuration.nix
+++ hosts/habitat/configuration.nix
@@ -282,6 +282,9 @@ in {
 
   networking.firewall.checkReversePath = "loose";
 
+  # Testing default for 23.05
+  services.nscd.enableNsncd = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
