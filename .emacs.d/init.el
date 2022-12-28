e306f23bdbca465051b748974153e410765b9fc5
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-12-29 00:20:18 +0200

    emacs: try out embark + update packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,11 +140,8 @@
   :config
   (enable-theme 'leuven))
 
-(use-package modus-themes
-  :if (not (display-graphic-p))
-  :init
+(unless (display-graphic-p)
   (load-theme 'modus-vivendi t t)
-  :config
   (enable-theme 'modus-vivendi))
 
 (use-package avy
@@ -211,9 +208,6 @@
   ; just hack at stuff.
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
-(straight-register-package
- '(vertico :host github :repo "minad/vertico"))
-
 (use-package vertico
   :init
   (vertico-mode))
@@ -255,6 +249,33 @@
 
   :init
   (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; consult version of my counsel-repo elisp.
 (defun consult-project ()
@@ -620,7 +641,9 @@
     (setq fill-column 72))
   (add-hook 'git-commit-mode-hook #'my-git-commit-mode-hook))
 
-(use-package git-timemachine)
+;; recipe still points to gitlab for git-timemachine
+(straight-use-package
+ '(git-timemachine :host codeberg :repo "pidu/git-timemachine"))
 
 (use-package browse-at-remote)
 
@@ -807,6 +830,12 @@
 (use-package expand-region
   :bind (("C-+" . er/contract-region)
          ("C-=" . er/expand-region)))
+
+(use-package mosey
+  :bind (("C-a" . mosey-backward-bounce)
+         ("C-e" . mosey-forward-bounce)))
+
+
 
 ;; trying out clojure
 (progn
