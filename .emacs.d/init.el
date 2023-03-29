e543feaebf547b3377cdebfc4732b22eeb9b03f2
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-03-29 09:34:37 +0200

    emacs: don't use detached for compile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -941,9 +941,6 @@
   (detached-init)
   :bind (;; Replace `async-shell-command' with `detached-shell-command'
          ([remap async-shell-command] . detached-shell-command)
-         ;; Replace `compile' with `detached-compile'
-         ([remap compile] . detached-compile)
-         ([remap recompile] . detached-compile-recompile)
          ;; Replace built in completion of sessions with `consult'
          ([remap detached-open-session] . detached-consult-session))
   :custom ((detached-show-output-on-attach t)
