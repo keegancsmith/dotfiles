c5dbcb4c5c6a60424feabeb634bc0e25073ef760
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-03-30 14:34:32 +0200

    nix: rename x11forwarding setting

--- hosts/habitat/configuration.nix
+++ hosts/habitat/configuration.nix
@@ -180,7 +180,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.forwardX11 = true;
+  services.openssh.settings.X11Forwarding = true;
 
   services.tailscale.enable = true;
 
