506041405cadcd21d0612533ea89768a22f0515b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-06-02 15:08:22 +0200

    nixos: enable hmac-sha2-512 for pass mobile app

--- hosts/habitat/configuration.nix
+++ hosts/habitat/configuration.nix
@@ -190,6 +190,15 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.X11Forwarding = true;
+  services.openssh.settings.Macs = [
+    # Defaults
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+
+    # pass mobile app requires this
+    "hmac-sha2-512"
+  ];
 
   services.tailscale.enable = true;
 
