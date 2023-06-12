36252aa4cd643ba6f5d4c44392ec7e37c2aaf01e
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-06-12 15:24:31 +0200

    counsel-repo: update to allow directly specifying org-files

--- .bashrc
+++ .bashrc
@@ -7,7 +7,7 @@ export HISTFILESIZE=80000
 export HISTSIZE=20000
 export MAILCHECK=0
 
-export SRCPATH="$HOME/src"
+export SRCPATH="$HOME/src:$HOME/org-files"
 
 # I hate it when I accidently lock the terminal in screen
 export LOCKPRG=/bin/true
--- lib/counsel-repo.nix
+++ lib/counsel-repo.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "counsel-repo";
-  version = "c052b44ad53fb906a78437e003d55991bedee0ab";
+  version = "bd34a0f2de1fa88b0640b6a511bcd0702e6f3105";
 
   src = fetchFromGitHub {
     owner = "keegancsmith";
     repo = "counsel-repo";
     rev = version;
-    sha256 = "sha256-pM7S3IPo09uHvueRtBgbWbnDBB3b9gnGiQkmi0mLAWo=";
+    sha256 = "sha256-67S9uiBOXkg/B7zXMTsK2FeLKOMeW0K9Z/IuTsHD8h0=";
   };
 
   vendorSha256 = null;
