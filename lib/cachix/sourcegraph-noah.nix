8936f4415ed4a6b46a1569da4f966e629cee1dc7
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-03-25 13:12:35 +0200

    nix: trust https://sourcegraph-noah.cachix.org

---
+++ lib/cachix/sourcegraph-noah.nix
@@ -0,0 +1,7 @@
+# This doesn't configure us to use the Sourcegraph cache, but rather makes it
+# possible to trust it for the sourcegraph repo's flake.nix
+{
+  nix.settings = {
+    trusted-substituters = [ "https://sourcegraph-noah.cachix.org" ];
+  };
+}
