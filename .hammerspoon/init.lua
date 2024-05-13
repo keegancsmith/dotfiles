51c7583a4f91525978abb8021ccd7d497e2dd801
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-05-13 16:59:11 +0200

    hammerspoon: okta via chrome

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -43,7 +43,8 @@ spoon.URLDispatcher.url_patterns = {
    { "https?://linear.app", "com.google.chrome" },
    { "https?://www.figma.com", "com.google.chrome" },
    { "https?://www.notion.so", "com.google.chrome" },
-   { "https?://www.loom.com", "com.google.chrome" }
+   { "https?://www.loom.com", "com.google.chrome" },
+   { "https?://sourcegraph.okta.com", "com.google.chrome" }
 }
 --spoon.URLDispatcher.logger.setLogLevel("debug")
 spoon.URLDispatcher:start()
