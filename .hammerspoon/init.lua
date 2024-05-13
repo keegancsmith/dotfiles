db7d0636831f21c82d5634cba629c603ef563021
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-05-13 09:08:11 +0200

    hammerspoon: update activate app hotkeys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,15 +6,10 @@ function activateFn(hint)
 end
 
 hs.hotkey.bind({ "alt" }, "1", activateFn("emacs"))
-hs.hotkey.bind({ "alt" }, "2", activateFn("safari"))
-hs.hotkey.bind({ "alt" }, "3", activateFn("slack"))
+hs.hotkey.bind({ "alt" }, "2", activateFn("qutebrowser"))
+hs.hotkey.bind({ "alt" }, "3", activateFn("code"))
 hs.hotkey.bind({ "alt" }, "4", activateFn("kitty"))
-
-hs.hotkey.bind({ "alt" }, "5", function()
-  local zoom = hs.application.find("zoom")
-  local win = zoom:findWindow("Zoom Meeting")
-  win:focus()
-end)
+hs.hotkey.bind({ "alt" }, "5", activateFn("slack"))
 
 hs.hotkey.bind({ "cmd", "shift" }, "v", function()
   hs.eventtap.keyStrokes(hs.pasteboard.getContents())
