69af55393391095ab4f38f99c944c39c0b4ad807
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-11-12 22:23:42 +0200

    nix: add sourcegraph cachix to trusted-public-keys
    For some reason on my macbook it isn't using them even when specified in
    the flake.nix.

--- lib/cachix/sourcegraph.nix
+++ lib/cachix/sourcegraph.nix
@@ -3,5 +3,6 @@
 {
   nix.settings = {
     trusted-substituters = [ "https://sourcegraph-noah.cachix.org" "https://sourcegraph-keegan.cachix.org" ];
+    trusted-public-keys = [ "sourcegraph-noah.cachix.org-1:rTTKnyuUmJuGt/UAXUpdOCOXDAfaO1AYy+/jSre3XgA=" "sourcegraph-keegan.cachix.org-1:cBc8JVINx349y/Ztd4ae2hp9Vb/tZRM5ZMjdjWCStlk=" ];
   };
 }
