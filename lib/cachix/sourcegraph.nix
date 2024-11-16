556dfdddf8e94d2958c67d23925d512718ff1d7d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-11-16 08:10:50 +0200

    nix: and ghostty.cachix

--- lib/cachix/sourcegraph.nix
+++ lib/cachix/sourcegraph.nix
@@ -1,5 +1,3 @@
-# This doesn't configure us to use the Sourcegraph cache, but rather makes it
-# possible to trust it for the sourcegraph repo's flake.nix
 {
   nix.settings = {
     trusted-substituters = [ "https://sourcegraph-noah.cachix.org" "https://sourcegraph-keegan.cachix.org" ];
