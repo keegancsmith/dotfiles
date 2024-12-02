139dc9bc09c4e7f84450fef0c12e3f56e0303f1d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-12-02 14:38:52 +0200

    emacs: smaller font on desktop

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,7 +38,7 @@
   (set-frame-font
    (pcase (system-name)
      ("fa.local" "JetBrains Mono Light 14")
-     (_          "JetBrains Mono Light 12"))
+     (_          "JetBrains Mono Light 10"))
    nil t)
   (setenv "LANG" "en_US.UTF-8")
   (setenv "LC_ALL" "en_US.UTF-8"))
