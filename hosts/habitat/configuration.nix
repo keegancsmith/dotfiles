779bcb2ed6aea48d07081bb0d5a2780e872607b9
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2025-01-16 15:59:12 +0200

    nixos: pin zoom to working version

--- hosts/habitat/configuration.nix
+++ hosts/habitat/configuration.nix
@@ -182,7 +182,15 @@
     volumeicon
     vscode
     yt-dlp
-    zoom-us
+
+    # Latest zoom hangs on nixos https://github.com/NixOS/nixpkgs/issues/371488
+    (zoom-us.overrideAttrs {
+      version = "6.2.11.5069";
+      src = pkgs.fetchurl {
+        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      };
+    })
   ]);
 
   fonts.packages = with pkgs; [
