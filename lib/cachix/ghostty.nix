03a5a58734431433ad509abb16e898d13262fa50
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2025-08-11 21:11:55 +0200

    nixos: add ghostty.cachix.org

---
+++ lib/cachix/ghostty.nix
@@ -0,0 +1,6 @@
+{
+  nix.settings = {
+    trusted-substituters = [ "https://ghostty.cachix.org" ];
+    trusted-public-keys = [ "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns=" ];
+  };
+}
