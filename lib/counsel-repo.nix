50543756dffb9af2a2fc3918e484e5dc2cd79719
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2025-10-21 09:28:12 +0200

    nix: bump counsel-repo

--- lib/counsel-repo.nix
+++ lib/counsel-repo.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "counsel-repo";
-  version = "bd34a0f2de1fa88b0640b6a511bcd0702e6f3105";
+  version = "3e7960bfd9b11d82055fb4cfc647570135f62671";
 
   src = fetchFromGitHub {
     owner = "keegancsmith";
     repo = "counsel-repo";
     rev = version;
-    sha256 = "sha256-67S9uiBOXkg/B7zXMTsK2FeLKOMeW0K9Z/IuTsHD8h0=";
+    sha256 = "sha256-T9WcSS2kts+G4sOPsco+Repf/0MJqUXebkYjwFAmWJc=";
   };
 
   vendorHash = null;
