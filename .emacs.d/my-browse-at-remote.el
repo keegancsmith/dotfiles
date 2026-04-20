ac5fab4fa231732cd0b751cea09afe414b8bea21
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2026-04-20 10:23:53 +0200

    emacs: browse-at-remote handles branches with /
    also c-u now uses the branch

--- .emacs.d/my-browse-at-remote.el
+++ .emacs.d/my-browse-at-remote.el
@@ -2,25 +2,22 @@
 
 (require 'browse-at-remote)
 
-(defun my-github-to-sourcegraph-url (github-url)
-  "Convert GITHUB-URL to Sourcegraph URL for sourcegraph repos."
-  (when (string-match "^\\(https?://\\)github\\.com/\\(sourcegraph/[^/]+\\)/blob/[^/]+/\\(.+\\)$" github-url)
-    (let* ((repo (match-string 2 github-url))
-           (path (match-string 3 github-url))
-           (path-and-lines (split-string path "#"))
-           (filepath (car path-and-lines))
-           (lines (cadr path-and-lines)))
-      (concat "https://sourcegraph.sourcegraph.com/github.com/" repo "/-/blob/" filepath
-              (when lines
-                (concat "?" (replace-regexp-in-string "L\\([0-9]+\\)-L\\([0-9]+\\)" "L\\1-\\2" lines)))))))
+(defun my-browse-at-remote--format-region-url-as-github-advice (orig-fn repo-url location filename &optional linestart lineend)
+  "Advice to produce Sourcegraph URLs for sourcegraph org repos.
+With a prefix argument, uses the current branch/commit (location) instead of main."
+  (if (string-match "github\\.com/\\(sourcegraph/[^/]+\\)" repo-url)
+      (let* ((repo (match-string 1 repo-url))
+             (branch (if current-prefix-arg location "main")))
+        (cond
+         ((and linestart lineend)
+          (format "https://sourcegraph.sourcegraph.com/github.com/%s@%s/-/blob/%s?L%d-%d" repo branch filename linestart lineend))
+         (linestart
+          (format "https://sourcegraph.sourcegraph.com/github.com/%s@%s/-/blob/%s?L%d" repo branch filename linestart))
+         (t
+          (format "https://sourcegraph.sourcegraph.com/github.com/%s@%s/-/blob/%s" repo branch filename))))
+    (funcall orig-fn repo-url location filename linestart lineend)))
 
-(defun my-browse-at-remote-get-url-advice (url)
-  "Advice to convert GitHub URLs to Sourcegraph for sourcegraph repos."
-  (if (string-match-p "sourcegraph" url)
-      (or (my-github-to-sourcegraph-url url) url)
-    url))
-
-(advice-add 'browse-at-remote-get-url :filter-return #'my-browse-at-remote-get-url-advice)
+(advice-add 'browse-at-remote--format-region-url-as-github :around #'my-browse-at-remote--format-region-url-as-github-advice)
 
 (provide 'my-browse-at-remote)
 
