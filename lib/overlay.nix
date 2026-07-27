0b72b3a9d7ced4ea71945281ae6939b652df9311
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2026-07-27 09:25:32 +0200

    nix flake update

--- lib/overlay.nix
+++ lib/overlay.nix
@@ -16,4 +16,14 @@ final: prev: rec {
   myEmacs = (prev.emacsPackagesFor prev.emacs30).emacsWithPackages (
     epkgs: [ epkgs.vterm epkgs.treesit-grammars.with-all-grammars ]
   );
+
+  # Backport nixpkgs 5f9ab4dd. The 1.98.9 bump retained the 1.98.5 vendor hash.
+  tailscale =
+    if prev.tailscale.version == "1.98.9" then
+      prev.tailscale.overrideAttrs
+        {
+          vendorHash = "sha256-Sd2iLJ7eDfDYdIRuW4xuiKgzhQWJWGAnz97FJWrVRlE=";
+        }
+    else
+      prev.tailscale;
 }
